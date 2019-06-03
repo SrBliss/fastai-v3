2c7792c8b6ca1a8535bf1e58db95dece83c653c2
SrBliss <45832838+SrBliss@users.noreply.github.com>
2019-06-03 18:30:22 +0200

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,10 +9,14 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
-export_file_name = 'export.pkl'
+# export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
+# export_file_name = 'export.pkl'
+# classes = ['black', 'grizzly', 'teddys']
 
-classes = ['black', 'grizzly', 'teddys']
+model_file_url = 'https://drive.google.com/uc?export=download&id=1QAVK-k2pRpjwkbH6wAWAJH7BG3ev44kI'
+model_file_name = 'monos-costarica.pkl'
+classes = ['araña', 'ardilla', 'aullador', 'capuchino']
+
 path = Path(__file__).parent
 
 app = Starlette()
